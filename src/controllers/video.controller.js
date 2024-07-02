a3f9e4cc0d15e05eca985195e38f2528e3e3176d
Rajkrishna debnath <rajkrishnadebnath53@gmail.com>
2024-07-02 21:16:17 +0530

    modified video.controller.js

--- src/controllers/video.controller.js
+++ src/controllers/video.controller.js
@@ -1,10 +1,11 @@
-import mongoose, {isValidObjectId, isValidObjectId} from "mongoose"
+import mongoose, {isValidObjectId} from "mongoose"
 import {Video} from "../models/video.model.js"
 import {User} from "../models/user.model.js"
 import {ApiError} from "../utils/ApiError.js"
 import {ApiResponse} from "../utils/ApiResponse.js"
 import {asyncHandler} from "../utils/asyncHandler.js"
 import {uploadOnCloudinary} from "../utils/cloudinary.js"
+import jwt from "jsonwebtoken"
 
 
 const getAllVideos = asyncHandler(async (req, res) => {
@@ -18,11 +19,11 @@ const publishAVideo = asyncHandler(async (req, res) => {
     // check that user is login or not 
     // check that all fields are available or not 
     // make a lockalpath for files and upload on cloudinary
-    // check that file lupload successfully with response like url and duration
+    // check that file upload successfully with response like url and duration
     // create a video document
     // check that document created successfully or not 
     
-    const user = await User.findById(req.user._id)
+    const user = await User.findById(req.user._id).select("-password -refreshToken")
 
     if(!user) throw new ApiError(400, "user is not found")
     
@@ -30,7 +31,32 @@ const publishAVideo = asyncHandler(async (req, res) => {
         throw new ApiError(400, "all fieds are required")
     }
 
+    const videoLocalPath = req.files?.videoFile[0]?.path
+    const thumbnailLocalPath = req.files?.thumbnail[0]?.path
+    if(!videoLocalPath && !thumbnailLocalPath) throw new ApiError(400, "video and thumbnail file is required")
+    
+    const video = await uploadOnCloudinary(videoLocalPath)
+    const thumbnail = await uploadOnCloudinary(thumbnailLocalPath)
 
+    if(!video && !thumbnail){
+        let error = "video"
+        if(video) error = "thoumbnail"
+        throw new ApiError(400, `${error} file is required from cloudinary`)
+    }
+
+    const videoDoc = await Video.create({
+        videoFile: video.url,
+        thumbnail: thumbnail.url,
+        title,
+        description,
+        duration: video.duration,
+        owner: user
+    })
+
+    if(!videoDoc) throw new ApiError(500, "Something went wrong while registering the video")
+
+    return res.status(200)
+    .json(200, {videoDoc}, "video registered successfully")
 })
 
 const getVideoById = asyncHandler(async (req, res) => {
