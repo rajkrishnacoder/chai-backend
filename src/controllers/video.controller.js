5d67e4e374a2dcaac903457b2d93becff7205852
Rajkrishna debnath <rajkrishnadebnath53@gmail.com>
2024-07-02 20:45:49 +0530

    update comment.controller.js, user.controller.js, video.controller.js and like.model.js

--- src/controllers/video.controller.js
+++ src/controllers/video.controller.js
@@ -1,4 +1,4 @@
-import mongoose, {isValidObjectId} from "mongoose"
+import mongoose, {isValidObjectId, isValidObjectId} from "mongoose"
 import {Video} from "../models/video.model.js"
 import {User} from "../models/user.model.js"
 import {ApiError} from "../utils/ApiError.js"
@@ -15,11 +15,36 @@ const getAllVideos = asyncHandler(async (req, res) => {
 const publishAVideo = asyncHandler(async (req, res) => {
     const { title, description} = req.body
     // TODO: get video, upload to cloudinary, create video
+    // check that user is login or not 
+    // check that all fields are available or not 
+    // make a lockalpath for files and upload on cloudinary
+    // check that file lupload successfully with response like url and duration
+    // create a video document
+    // check that document created successfully or not 
+    
+    const user = await User.findById(req.user._id)
+
+    if(!user) throw new ApiError(400, "user is not found")
+    
+    if([title, description].some((field) => field?.trim() == "")){
+        throw new ApiError(400, "all fieds are required")
+    }
+
+
 })
 
 const getVideoById = asyncHandler(async (req, res) => {
     const { videoId } = req.params
     //TODO: get video by id
+    const isValidObjectId = isValidObjectId(videoId)
+    if(!isValidObjectId) throw new ApiError(400, "video id required")
+
+    const video = await Video.findById(videoId)
+
+    if(!video) throw new ApiError(400, "video is not found")
+
+    return res.status(200)
+    .json(200, {video}, "video is fetched successfully")
 })
 
 const updateVideo = asyncHandler(async (req, res) => {
