2271b1e3355cf4d71e4a9ed26408f0bf9f505ed7
Rajkrishna debnath <rajkrishnadebnath53@gmail.com>
2024-07-04 21:42:28 +0530

    updating index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,8 @@
 // require('dotenv').config({path: './env'})
-import dotenv from "dotenv"
 import connectDB from "./db/index.js";
+import dotenv from "dotenv"
 import {app} from './app.js'
-dotenv.config({
-    path: './.env'
-})
+dotenv.config({path: "./.env"})
 
 
 
@@ -17,9 +15,6 @@ connectDB()
 .catch((err) => {
     console.log("MONGO db connection failed !!! ", err);
 })
-
-
-
 
 
 
