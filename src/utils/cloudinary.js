6d6e9ebdca4f0c6bce952501b3431fdb5c3dcf2f
Rajkrishna debnath <rajkrishnadebnath53@gmail.com>
2024-07-05 18:33:18 +0530

    update

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -21,6 +21,7 @@ const uploadOnCloudinary = async (localFilePath) => {
         return response;
 
     } catch (error) {
+        console.log("ERROR:", error)
         fs.unlinkSync(localFilePath) // remove the locally saved temporary file as the upload operation got failed
         return null;
     }
